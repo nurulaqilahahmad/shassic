aedbc556ae790f2e461280e6b274a273821bce6d
Nurul Aqilah Ahmad <naqilahahmad@yahoo.com>
2022-10-18 11:35:45 +0800

    Update User Log In

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if (isset($_POST['login'])) {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>SHASSIC | Log In</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
