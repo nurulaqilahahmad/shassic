87ccdeae5f94eed9c805df88b077d5ad15aced56
syiqinyusof <syiqinyusof@gmail.com>
2022-10-25 10:51:29 +0800

    Update Remember Me Feature

--- login.php
+++ login.php
@@ -1,22 +1,44 @@
 <?php
 session_start();
 include('includes/config.php');
+if (isset($_COOKIE['email']) && isset($_COOKIE['password'])) {
+    $email = $_COOKIE['email'];
+    $password = $_COOKIE['password'];
+} else {
+    $email = "";
+    $password = "";
+}
 
 if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     $sql = "SELECT * FROM user WHERE email = ?";
-    $result = $dbh->prepare($sql);
-    $result->bindParam(1, $email);
-    $result->execute();
+    $query = $dbh->prepare($sql);
+    $query->bindParam(1, $email);
+    $query->execute();
 
-    $user = $result->fetch();
-
-    if (password_verify($password, $user['password'])) {
-        echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
-    } else {
-        echo "<script>alert('Incorrect Email Address or Password');</script>";
+    $result = $query->fetch();
+    //$result=mysqli_query($con, $sql);
+    if ($result) {
+        if ($query->rowCount() > 0) {
+        //if(mysqli_num_rows($result)==1){
+            $result_fetch = mysqli_fetch_assoc($result);
+            if (password_verify($password, $result['password'])) {
+                $_SESSION['login'] = true;
+                $_SESSION['email'] = $result_fetch['email'];
+                if (isset($_POST['remember_me'])) {
+                    setcookie('email', $_POST['email'], time() + (60 * 60 * 24));
+                    setcookie('password', $_POST['password'], time() + (60 * 60 * 24));
+                } else {
+                    setcookie('email', '', time() - (60 * 60 * 24));
+                    setcookie('password', '', time() - (60 * 60 * 24));
+                }
+                echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
+            } else {
+                echo "<script>alert('Incorrect Email Address or Password');</script>";
+            }
+        }
     }
 }
 ?>
@@ -64,14 +86,14 @@ if (isset($_POST['login'])) {
                                     </div>
                                     <form class="user" action="login.php" method="post">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="email" name="email" aria-describedby="emailHelp" placeholder="Email Address" required>
+                                            <input type="email" class="form-control form-control-user" id="email" name="email" aria-describedby="emailHelp" placeholder="Email Address" required value="<?php echo $email ?>">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password" required>
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password" required value="<?php echo $password ?>">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck" name="remember_me">
                                                 <label class="custom-control-label" for="customCheck">Remember
                                                     Me</label>
                                             </div>
@@ -96,6 +118,43 @@ if (isset($_POST['login'])) {
         </div>
 
     </div>
+
+    <!-- <script type="text/javascript">
+        function setcookie() {
+            var u = document.getElementById('email').value;
+            var p = document.getElementById('password').value;
+
+            document.cookie = "myemail=" + u + ";path=http://localhost/shassic/";
+            document.cookie = "mypswd=" + p + ";path=http://localhost/shassic/";
+        }
+
+        function getcookiedata() {
+
+            console.Log(document.cookie);
+
+            var user = getCookie('myemail');
+            var pswd = getCookie('mypswd');
+
+            document.getElementById('email').value = user;
+            document.getElementById('password').value = pswd;
+        }
+
+        function getCookie(cname) {
+            var name = cname + "=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var ca = decodedCookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.chartAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+    </script> -->
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
